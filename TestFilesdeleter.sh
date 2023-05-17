c632af79e714f7897d9d0f9904566f2164147659
Artem09076 <120830078+Artem09076@users.noreply.github.com>
2023-05-17 16:36:11 +0300

    Update TestFilesdeleter.sh

--- TestFilesdeleter.sh
+++ TestFilesdeleter.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-chmod +x Filesdeleter.sh
-./Filesdeleter.sh Тенигин test1.txt
+`chmod +x Filesdeleter.sh`
+`./Filesdeleter.sh Тенигин test1.txt`
 if [[ ! -f test1.txt ]]; then
 	exit 1
 fi
 
-./Filesdeleter.sh Тенигин test2.txt
+`./Filesdeleter.sh Тенигин test2.txt`
 if [[ -f test2.txt ]]; then
 	exit 1
 fi
-./Filesdeleter.sh Тенигин Тенигин.txt
+`./Filesdeleter.sh Тенигин Тенигин.txt`
 if [[ ! -f Тенигин.txt ]]; then
 	exit 1
 fi
