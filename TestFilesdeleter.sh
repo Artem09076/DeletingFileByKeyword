87df60390f1aa2da2fae8cc2e6904ab5d857af7f
Artem09076 <120830078+Artem09076@users.noreply.github.com>
2023-05-17 16:22:31 +0300

    Add files via upload

---
+++ TestFilesdeleter.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+chmod +x Filesdeleter.sh
+./Filesdeleter.sh Тенигин test1.txt
+if [[ ! -f test1.txt ]]; then
+	exit 1
+fi
+
+./Filesdeleter.sh Тенигин test2.txt
+if [[ -f test2.txt ]]; then
+	exit 1
+fi
+./Filesdeleter.sh Тенигин Тенигин.txt
+if [[ ! -f Тенигин.txt ]]; then
+	exit 1
+fi
