b8cf354995f81c514875ddf77ea2d51c1f611aae
Artem09076 <120830078+Artem09076@users.noreply.github.com>
2023-05-19 22:20:08 +0300

    Update TestFilesdeleter.sh

--- TestFilesdeleter.sh
+++ TestFilesdeleter.sh
@@ -4,16 +4,16 @@ chmod +x Filesdeleter.sh
 touch test1.txt
 echo "Тенигин Альберт Андреевич"> test1.txt
 if [[ ! -f test1.txt ]]; then
-	echo "Test 1 passed"
+	echo "---------Test 1 passed---------"
 fi
 touch test2.txt
 echo "Шабрашин Иван Геннадьевич"> test2.txt
 ./Filesdeleter.sh Тенигин test2.txt
 if [[ -f test2.txt ]]; then
-	echo "Test 2 passed"
+	echo "---------Test 2 passed---------"
 fi
 touch Тенигин.txt
 ./Filesdeleter.sh Тенигин Тенигин.txt
 if [[ ! -f Тенигин.txt ]]; then
-	echo "Test 3 passed"
+	echo "---------Test 3 passed---------"
 fi
