03151c35702727c06eb9dc9ce21db1bc9d283b87
Artem09076 <120830078+Artem09076@users.noreply.github.com>
2023-05-20 19:17:55 +0300

    Update TestFilesdeleter.sh

--- TestFilesdeleter.sh
+++ TestFilesdeleter.sh
@@ -5,15 +5,21 @@ echo "Тенигин Альберт Андреевич"> test1.txt
 ./Filesdeleter.sh Тенигин test1.txt
 if [[ ! -f test1.txt ]]; then
 	echo "---------Test 1 passed---------"
+else
+	exit 0
 fi
 touch test2.txt
 echo "Шабрашин Иван Геннадьевич"> test2.txt
 ./Filesdeleter.sh Тенигин test2.txt
 if [[ -f test2.txt ]]; then
 	echo "---------Test 2 passed---------"
+else
+	exit 0
 fi
 touch Тенигин.txt
 ./Filesdeleter.sh Тенигин Тенигин.txt
 if [[ ! -f Тенигин.txt ]]; then
 	echo "---------Test 3 passed---------"
+else
+	exit 0
 fi
